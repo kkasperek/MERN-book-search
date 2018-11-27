a58512b2a22f7dfa2035e8e032f8d69757a1206e
kkasperek <kt.kasperek@gmail.com>
2018-11-27 16:47:01 -0600

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Bookshelf from "./pages/bookshelf";
 import Search from "./pages/search";
-import NavBar from "./components/Navigation/NavBar";
+import NavBar from "./components/NavBar/NavBar";
 
 const App = () => (
   <Router>
--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -41,9 +40,6 @@ function NavBar(props) {
   );
 }
 
-NavBar.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
 
 export default withStyles(styles)(NavBar);
 
